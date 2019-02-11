7a2e1044213888b8c3e54c27c52f4f1ca7575400
vinipx <vinipeixotofagundes@gmail.com>
2019-02-11 21:53:41 +0100

    ST - add firstTest

---
+++ src/SongBaseTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SongBaseTest {
+
+    @Test
+    public void firstTest(){
+        assertEquals(1,1);
+    }
+}
